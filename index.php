a934f2cb0e30be0868d72106d4d6cb50334717e4
Amanda Reahana Gomes <reahana233@gmail.com>
2021-10-26 00:08:05 +0600

    Index log in file

--- index.php
+++ index.php
@@ -2,15 +2,16 @@
 <html>
 <head>
 	<meta charset="utf-8" name="viewport" content="width=device-width,intial-scale=1">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <title>Library Management System</title>
+	<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
+	<link rel="stylesheet" href="bootstrap-5.1.3-dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
+	<title>Library Management System</title>
 </head>
 <body>
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 		<div class="container-fluid">
 			<div class="navbar-header">
-				<a class="navbar-brand" href="index.php">Library Management System(LMS)</a>
+				<a class="navbar-brand" href="index.php">Library Management System (LMS)</a>
 			</div>
 			<ul class="nav navbar-nav navbar-right">
 				<li class="nav-item">
@@ -25,16 +26,16 @@
 			</ul>
 		</div>
 	</nav><br>
-	<span><marquee>This is library Management System. Library opens at 8:00 AM and close at 8:00 PM</marquee></span><br><br>
+	<span><marquee>This is Library Management System. Library opens at 8:00 AM and close at 8:00 PM</marquee></span><br><br>
 	<div class="row">
 		<div class="col-md-4" id="side_bar">
-			<h5>Library Timing</h5>
+			<h5 style="color:blue">Library Timing</h5>
 			<ul>
 				<li>Opening Timing: 8:00 AM</li>
 				<li>Closing Timing: 8:00 PM</li>
 				<li class="timing">(Friday off)</li>
 			</ul>
-			<h5>What we provide ?</h5>
+			<h5 style="color:green">What we provide ?</h5>
 			<ul>
 				<li>Full furniture</li>
 				<li>Free Wi-fi</li>
@@ -86,6 +87,7 @@
 		</div>
 			
 	</div>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
+	<script src="bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
