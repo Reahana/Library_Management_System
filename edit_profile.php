2cb7f462470253a1a3ba6894a926d27cfd38a10c
Reahana <reahana233@gmail.com>
2024-04-04 19:43:42 +0600

    edit profile

--- edit_profile.php
+++ edit_profile.php
@@ -15,16 +15,13 @@
 		$address = $row['address'];
 	}
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Edit User Profile</title>
-	<meta charset="utf-8" name="viewport" content="width=device-width,intial-scale=1">
-	<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
-    <link rel="stylesheet" href="bootstrap-5.1.3-dist/css/bootstrap.min.css">
-	<link rel="stylesheet" href="style.css">
+
+<?php include("header.php") ?>
+<title>Edit User Profile</title>
 </head>
 <body>
+
+<div class="template bg-light">
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 		<div class="container-fluid">
 			<div class="navbar-header">
@@ -49,7 +46,7 @@
 	<div class="row">
 		<div class="col-md-4"></div>
 		<div class="col-md-4">
-			<div class="container">
+			<div class="container ">
 			<form action="update.php" method="post">
 				<div class="form-group mb-2">
 					<label>Name:</label>
@@ -67,13 +64,10 @@
 					<label>Address:</label>
 					<textarea rows="3" cols="40" name="address" class="form-control"><?php echo $address;?></textarea>
 				</div> <br>
-				<button  type="submit" name="update" class="btn btn-warning">Update</button>
+				<button  type="submit" name="update" class="btn btn-warning ">Update</button>
 			</form>
 			</div>
 		</div>
 		<div class="col-md-4"></div>
 	</div>
-	<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
-	<script src="bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+<?php include("footer.php") ?>
